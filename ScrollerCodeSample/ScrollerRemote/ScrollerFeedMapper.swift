cf4024b8f6385490cd60dd5e1751cc4855e479f1
Kyle Vigorito <kyle.vigorito@gmail.com>
2022-03-09 10:36:26 -0500

    Modified ScrollerFeedMapper to return RemoteFeedImages directly

--- ScrollerCodeSample/ScrollerRemote/ScrollerFeedMapper.swift
+++ ScrollerCodeSample/ScrollerRemote/ScrollerFeedMapper.swift
@@ -8,38 +8,16 @@
 import Foundation
 final class ScrollerFeedMapper {
     private struct Root: Decodable {
-        let items: [Item]
-
-        var feed: [ScrollerFeedImage] {
-            return items.map { $0.item }
-        }
-    }
-
-    private struct Item: Decodable {
-        let id: UUID
-        let description: String?
-        let location: String?
-        let image: URL
-
-        var item: ScrollerFeedImage {
-            return ScrollerFeedImage(id: id, description: description, location: location, url: image)
-        }
-
-        enum CodingKeys: String, CodingKey {
-            case id = "image_id"
-            case description = "image_desc"
-            case location = "image_loc"
-            case image = "image_url"
-        }
+        let items: [RemoteFeedImage]
     }
 
     static let VALID_CODES = [200]
 
-    static func map(_ data: Data, from response: HTTPURLResponse) -> RemoteScrollerFeedLoader.Result {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteFeedImage] {
         guard VALID_CODES.contains(response.statusCode), let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(RemoteScrollerFeedLoader.Error.invalidData)
+            throw RemoteScrollerFeedLoader.Error.invalidData
         }
 
-        return .success(root.feed)
+        return root.items
     }
 }
